6aab88bf841622e6ea766cb7ad886804c76d6a14
IliyanGavrilov <wardheromgrtoxic@gmail.com>
2026-06-02 00:31:00 +0300

    Remove unused entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" \
-    < /var/www/html/json_converter/sql/setup.sql \
-    || echo "DB setup skipped (tables may already exist)"
-
-exec apache2-foreground
